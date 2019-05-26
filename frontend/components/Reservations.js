bf0aa16622e5dc4b5252ac9b200c1c298b9a806f
Eric Bayasgalan <ewiz1020@gmail.com>
2019-05-26 00:38:40 -0700

    added reservations component

--- frontend/components/Reservations.js
+++ frontend/components/Reservations.js
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import Error from "./ErrorMessage";
-import { CURRENT_USER_QUERY } from "./User";
+import { perPage } from "../config";
 
-const RESERVATIONS_QUERY = gql`
-  query RESERVATIONS_QUERY { 
-    reservations() {
+const ALL_RESERVATIONS_QUERY = gql`
+  query ALL_RESERVATIONS_QUERY($skip: Int = 0, $first: Int = ${perPage}) { 
+    reservations(first: $first, skip: $skip, orderBy: createdAt_DESC ) {
       id
       checkIn
       checkOut
@@ -15,14 +13,28 @@ const RESERVATIONS_QUERY = gql`
   }
 `;
 
-class Reservations extends Component {
-  render() {
-    return (
-      <Query query={}>
-        
+function Reservations(props) {
+  return (
+      <Query
+        query={ALL_RESERVATIONS_QUERY}
+        variables={{
+          skip: props.page * perPage - perPage
+        }}
+      >
+        {({ data, error, loading }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error: {error.message}</p>;
+          return (
+            <ReservationsList>
+              {data.reservations.map(reservation => (
+
+              ))}
+            </ReservationsList>
+          );
+        }}
       </Query>
-    )
-  }
+  );
 }
 
 export default Reservations;
+export { ALL_RESERVATIONS_QUERY };
