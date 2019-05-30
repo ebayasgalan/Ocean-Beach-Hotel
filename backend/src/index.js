1e40f420004971e4ab16618d71bd57ea7cf07c37
Eric Bayasgalan <ewiz1020@gmail.com>
2019-05-29 17:12:39 -0700

    worked on CORS origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,14 +19,25 @@ server.express.use((req, res, next) => {
 });
 
 // Express middleware to populate current user
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    {
+      where: { id: req.userId }
+    },
+    "{id, email, name}"
+  );
+  req.user = user;
+  next();
+});
 
 server.start(
-  // {
-  //   cors: {
-  //     credentials: true,
-  //     origin: process.env.FRONTEND_URL
-  //   }
-  // },
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.PRISMA_ENDPOINT
+    }
+  },
   deets => {
     console.log(`Server is running on port http:/localhost:${deets.port}`);
   }
